770f8d5cb4c3977f2e9442c5fae44163ac1a9db6
zbuck1 <95191417+zbuck1@users.noreply.github.com>
2021-12-14 12:36:02 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "Cecilia is the champion";
+document.body.append(newHeader);
